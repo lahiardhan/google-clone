5f71838a896eb67f8d97e23f5f483cbac19e9a8a
Muhammad Lahia Ardhan <lahia.ardhanm@gmail.com>
2022-06-27 14:41:54 +0700

    menambahkan bagian avatar dan footer

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,27 @@
 import Head from 'next/head'
-import Avatar from '../components/Avatar/Avatar'
 import Image from 'next/image'
 import { MicrophoneIcon, ViewGridIcon } from '@heroicons/react/solid'
 import { SearchIcon } from '@heroicons/react/outline'
+import Avatar from '../components/organisms/Avatar'
+import Footer from '../components/organisms/Footer'
+import { useRef } from 'react'
+import { useRouter } from 'next/router';
 
 export default function Home() {
+  const router = useRouter();
+  const searchInputRef = useRef(null);
+
+  const search = (e) => {
+    e.preventDefault();
+    const term = searchInputRef.current.value;
+
+    if(!term) return;
+    
+    router.push(`/search?term=${term}`)
+  }
+
   return (
-    <div>
+    <div className='flex flex-col justify-center h-screen'>
       <Head>
         <title>Google</title>
         <link rel="icon" href="/favicon.ico" />
@@ -34,21 +49,32 @@ export default function Home() {
       </header>
 
       {/* Body */}
-      <form>
+      <form className='flex flex-col flex-grow items-center justify-center'>
         <Image 
           src='https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-google-sva-scholarship-20.png'
           height={100}
           width={300}
-          alt='google_logo'
+          alt='google-logo'
         />
         <div className='flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 sm:max-w-xl lg:max-w-2xl' >
           <SearchIcon className='h-5 mr-3 text-gray-500' />
-          <input type='text' className='flex-grow focus:outline-none' />
-          <MicrophoneIcon className='h-5 ml-3 text-gray-500' />
+          <input 
+            type='text' 
+            ref={searchInputRef}
+            className='flex-grow focus:outline-none' 
+          />
+          <MicrophoneIcon className='h-5 ml-3 text-gray-500 cursor-pointer' />
+        </div>
+
+        <div className='flex flex-col w-1/2 space-y-2 justify-center sm:flex-row sm:space-y-0 sm:space-x-4'>
+          <button onClick={search} className='btn'>Google Search</button>
+
+          <button onClick={search} className='btn'>I`m Feeling Lucky</button>
         </div>
       </form>
 
       {/* Footer */}
+      <Footer />
     </div>
   )
 }
