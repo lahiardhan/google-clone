0ea2c739246e47fd42ecc304e16ddee20d6fda23
Muhammad Lahia Ardhan <lahia.ardhanm@gmail.com>
2022-06-30 11:29:11 +0700

    memindahkan avatar ke dalam folder atom components

---
+++ components/atoms/Avatar/index.js
@@ -0,0 +1,15 @@
+/* eslint-disable @next/next/no-img-element */
+import React from 'react'
+
+function Avatar({url, className}) {
+  return (
+    <img 
+      className={`h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110 ${className}`}
+      loading='lazy'
+      src={url}
+      alt='profile picture'
+   />
+  )
+}
+
+export default Avatar
