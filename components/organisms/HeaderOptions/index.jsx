2ee63d9829773228f797984989ed169ddb000d51
Muhammad Lahia Ardhan <lahia.ardhanm@gmail.com>
2022-07-03 17:50:23 +0700

    add header options item

--- components/organisms/HeaderOptions/index.jsx
+++ components/organisms/HeaderOptions/index.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import HeaderOptionItem from "./HeaderOptionItem";
+import { 
+   DotsVerticalIcon,
+   MapIcon,
+   NewspaperIcon,
+   PhotographIcon,
+   PlayIcon,
+   SearchIcon
+} from "@heroicons/react/outline";
 
 function HeaderOptions() {
 	return( 
-      <div>
+      <div className="flex w-full text-gray-700 justify-evenly text-sm lg:text-base lg:justify-start lg:space-x-36 lg:pl-52 border-b">
          {/* Left Section */}
          <div className="flex space-x-6">
-            
+            <HeaderOptionItem Icon={SearchIcon} title='All' selected />
+            <HeaderOptionItem Icon={PhotographIcon} title='Images' />
+            <HeaderOptionItem Icon={PlayIcon} title='Videos' />
+            <HeaderOptionItem Icon={NewspaperIcon} title='News' />
+            <HeaderOptionItem Icon={MapIcon} title='Maps' />
+            <HeaderOptionItem Icon={DotsVerticalIcon} title='More' />
          </div>
 
          {/* Right Section */}
