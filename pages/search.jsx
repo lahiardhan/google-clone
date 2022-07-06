589118a7722950388a16391f3831eee80997ea1c
Muhammad Lahia Ardhan <lahia.ardhanm@gmail.com>
2022-07-06 11:41:25 +0700

    finsih the search results function

--- pages/search.jsx
+++ pages/search.jsx
@@ -28,15 +28,13 @@ function Search({results}) {
 export default Search;
 
 export async function getServerSideProps(context) {
-  const useDummyData =true;
+  const useDummyData = false;
   const startIndex = context.query.start || '0';
 
-  const data = useDummyData ? Response : await fetch(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`)
-                          .then(response => response.json());
+  const data = useDummyData ? Response : await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
+  ).then((response) => response.json());
 
-  // const data = useDummyData ? Response : await axios.get(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`)
-  //                         .then(response => response.json());
-  
   // After the SERVER has rendered... Pass the results to the client...
   return {
     props: {
